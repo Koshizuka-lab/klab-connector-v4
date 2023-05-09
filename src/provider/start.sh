fc6d6c6419a625ee8adc43e533326f1c6d83f7dc
Freddy-me-me2 <55520073+Freddy-me-me2@users.noreply.github.com>
2023-05-09 14:49:17 +0900

    Update start.sh
    デタッチモードを使わないようにした（ログが見えるように）

--- src/provider/start.sh
+++ src/provider/start.sh
@@ -4,11 +4,15 @@ start_type=$1
 if [ "$start_type" = "noproxy" ]
 then
     # リバースプロキシを起動しない
-    docker compose -f docker-compose_noproxy.yml up -d
+    # docker compose -f docker-compose_noproxy.yml up -d
+    # デタッチしない
+    docker compose -f docker-compose_noproxy.yml up
     docker compose ps
 
 else
     # 全コンテナを起動
+    # docker compose -f docker-compose.yml up -d
+    # デタッチしない
     docker compose -f docker-compose.yml up -d
     docker compose ps
 

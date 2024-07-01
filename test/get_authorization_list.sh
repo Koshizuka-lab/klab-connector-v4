88ee518c31079db57710ebddc81eaa7c59cdced3
mitk23 <mitk232@gmail.com>
2024-07-01 16:27:20 +0900

    update test scripts for provider env.

--- test/get_authorization_list.sh
+++ test/get_authorization_list.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-source ./.env
+source .env.provider
+
+BASIC=$(echo -n "${CLIENT_ID}:${CLIENT_SECRET}" | base64)
 
 # Login
-read -p "利用者ユーザID: " consumer_user_id
-read -p "パスワード: " password
+read -p "User ID: " consumer_user_id
+read -p "Password: " password
 
 BODY=$(cat << EOS | jq
 {
   "user_id": "$consumer_user_id",
-  "password": "$password",
-  "client_id": "consumer1_webapp",
-  "client_secret": "X0IwpZHnuFI8uduRkM5RV5A8F1XJwF3T"
+  "password": "$password"
 }
 EOS
 )
-TOKEN=$(curl -X POST "$TOKEN_API" -sS -H "Content-Type: application/json" -d "$BODY" --cacert "$CA_CERT" | jq -r '.access_token')
-echo "$TOKEN"
-echo ""
+TOKEN=$(curl -X POST "$TOKEN_API" -sS -H "Content-Type: application/json" -H "Authorization: Basic $BASIC" -d "$BODY" --cacert "$CA_CERT" | jq -r '.access_token')
+echo "Successfully authenticated."
+echo
 
-# data exchange
-read -p "提供者ユーザID: " provider_user_id
+# list authorization policies
+read -p "Provider User ID: " provider_user_id
 
-curl -v -G "$AUTHORIZATION_LIST_API" -s -S \
+result=$(curl -v -X GET "${AUTHZ_LIST_API}?assigner=${provider_user_id}" -sS \
 -H "Authorization:Bearer $TOKEN" \
--d "assigner=${provider_user_id}" \
 --cacert "$CA_CERT" \
-| jq '.'
+--cert "$CLIENT_CERT" \
+--key "$CLIENT_KEY")
+
+if command -v jq &> /dev/null; then
+    echo "$result" | jq '.'
+else
+    echo "$result"
+fi
